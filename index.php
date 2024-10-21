8e9ecd7b10337d5c7f6a5e9245f39397c37baed0
Steve Montgomery <steve@chums.com>
2024-10-21 17:58:58 -0600

    fix: remove unused imports

--- index.php
+++ index.php
@@ -3,13 +3,6 @@
 use chums\ui\WebUI2;
 use chums\user\Groups;
 
-/**
- * @package Chums
- * @subpackage ProjectedDemands
- * @author Steve Montgomery
- * @copyright Copyright &copy; 2013, steve
- */
-
 require_once("autoload.inc.php");
 
 $ui = new WebUI2([
@@ -21,19 +14,3 @@ $ui = new WebUI2([
 $ui->addCSS('public/css/styles.css', \chums\ui\CSSOptions::parse(['useTimestampVersion' => true]))
     ->addManifestJSON('public/js/manifest.json')
     ->render();
-//
-//$bodyPath = "apps/b2b-scoreboard";
-//$title = "B2B Scoreboard";
-//$description = "";
-//
-//$ui = new WebUI($bodyPath, $title, $description, true, 5);
-//$ui->version = "2019-03-07";
-//$ui->bodyClassName = 'container-fluid';
-//$ui->AddCSS("public/css/styles.css");
-//$ui->addManifest('public/js/manifest.json');
-///**
-// * Changelog:
-// */
-//
-//
-//$ui->Send();
